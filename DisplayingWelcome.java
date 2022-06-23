a3e228f49c3ce8288c5297b3e9b36bc1f46b4305
“Nagamani11” <nagamanidn95@gmail.com>
2022-06-23 10:20:25 +0530

    First commit

---
+++ DisplayingWelcome.java
@@ -0,0 +1,8 @@
+package Day3_PracticeProblems;
+
+public class DisplayingWelcome {
+	public static void main(String args[]) {
+	System.out.println("Welcome to Employee Wage Computation Program");
+
+}
+}
